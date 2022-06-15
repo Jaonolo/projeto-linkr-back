8394f838abbbdc8d1725fd6bb57a42ff8381f028
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-06-14 22:46:52 -0300

    fix: send info user post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,34 +9,32 @@ export async function getPostByUser(req,res){
         return res.sendStatus(422);
     }
 
-    const authorization = req.headers.authorization;
-    const token = authorization?.replace("Bearer ", "").trim();
+    //const authorization = req.headers.authorization;
+    //const token = authorization?.replace("Bearer ", "").trim();
 
-    if(!token){
-        return res.sendStatus(401);
-    }
+    //if(!token){
+        //return res.sendStatus(401);
+    //}
 
     try {
 
-        const userAuthorized = await db.query(`SELECT * FROM sessions WHERE "token"=$1 and 
-                                                        "userId"=$2`, [token, parseInt(req.params.id)]);
-        if(userAuthorized.rowCount == 0){
-            return res.sendStatus(401);
-        }
+        //const userAuthorized = await db.query(`SELECT * FROM sessions WHERE "token"=$1 and 
+                                                        //"userId"=$2`, [token, parseInt(req.params.id)]);
+        //if(userAuthorized.rowCount == 0){
+            //return res.sendStatus(401);
+        //}
 
         const isUser = await db.query(`SELECT * FROM users WHERE id=$1`, [parseInt(req.params.id)])
         if(isUser.rowCount == 0){
             return res.sendStatus(404);
         }
 
-        const postsInfo = await db.query(`SELECT users."userName", users."profilePicture", 
-                                                            posts.message, posts.link, COUNT(likes."postId") as likes
-                                                            FROM users
-                                                            JOIN posts ON posts."userId" = users.id
-                                                            LEFT JOIN likes ON posts.id = likes."postId" 
-                                                            WHERE users.id=$1
-                                                            GROUP BY users."userName", users."profilePicture",
-                                                            posts.message, posts.link`, [parseInt(req.params.id)]);
+        const postsInfo = await db.query(`SELECT posts.message, posts.link, COUNT(likes."postId") as likes
+                                            FROM posts
+                                            JOIN users ON posts."userId" = users.id
+                                            LEFT JOIN likes ON posts.id = likes."postId" 
+                                            WHERE users.id=$1
+                                            GROUP BY posts.message, posts.link`, [parseInt(req.params.id)]);
         
         const hashtags = await db.query(`SELECT COUNT("hashtagId") as hashtagCount, hashtags.tag FROM "postsHashtags"
         JOIN hashtags ON "postsHashtags"."hashtagId" = hashtags.id
@@ -45,8 +43,11 @@ export async function getPostByUser(req,res){
         LIMIT 10`);
         
         const sendPostInfo = {
-            postsinfo: postsInfo.rows,
-            hashtagsInfo: hashtags.rows
+            id: isUser.rows[0].id,
+            userName: isUser.rows[0].userName,
+            profilePicture: isUser.rows[0].profilePicture,
+            postsInfo: postsInfo.rows,
+            allHashtagsInfo: hashtags.rows
         }
 
         res.status(200).send(sendPostInfo);
